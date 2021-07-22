52265b7e8b670dbe4889112f5d410e2f668059ca
Askar Rizvi <askarrizvi88@gmail.com>
2021-07-22 15:02:07 -0400

    Change inquirer.prompt to be called from the init function rather than in the question array

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,14 +1,68 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  var licbadge;
+  if(license==='GNU GPLv3'){
+    licbadge = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+  }
+  else if(license==='Mozilla Public License 2.0'){
+    licbadge = "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+  }
+  else if(license==='Apache License 2.0'){
+    licbadge = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+  }
+  else if(license==='MIT License'){
+    licbadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  }
+  else{
+    licbadge = "";
+  }
+  return licbadge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  var liclink;
+  if(license==='GNU GPLv3'){
+    liclink = "https://www.gnu.org/licenses/gpl-3.0";
+  }
+  else if(license==='Mozilla Public License 2.0'){
+    liclink = "https://opensource.org/licenses/MPL-2.0";
+  }
+  else if(license==='Apache License 2.0'){
+    liclink = "https://opensource.org/licenses/Apache-2.0";
+  }
+  else if(license==='MIT License'){
+    liclink = "https://opensource.org/licenses/MIT";
+  }
+  else{
+    liclink = "";
+  }
+  return liclink;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  var licsec;
+  if(license==='GNU GPLv3'){
+    licsec = "Licensed under GNU GPLv3";
+  }
+  else if(license==='Mozilla Public License 2.0'){
+    licsec = "Licensed under Mozilla Public License 2.0";
+  }
+  else if(license==='Apache License 2.0'){
+    licsec = "Licensed under Apache License 2.0";
+  }
+  else if(license==='MIT License'){
+    licsec = "Licensed under MIT License";
+  }
+  else{
+    licsec = "";
+  }
+  return licsec;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
